b09b1b7d0d511fc18e8aaff942b4a5cba4d1fe41
root <liorshahverdi@gmail.com>
2017-09-18 17:42:12 -0400

    removed unused packages

--- forecast.py
+++ forecast.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import linear_model
 
 def read_and_trim_training_data_from_csv(filename):
 	table = []
